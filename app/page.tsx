a9bea8ed73bb68847a2e59a1281166a0580e9551
johnrysmalabanan071-svg <johnrysmalabanan071@gmail.com>
2026-09-17 06:32:06 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import { Portfolio } from '../components/portfolio';
+import { Portfolio } from "../components/portfolio";
+import AIGridBackground from "../components/AIGridBackground";
 
-export default function Home() {
-  return <Portfolio />;
+export default function HomePage() {
+  return (
+    <main className="relative isolate min-h-screen">
+      <AIGridBackground position="fixed" className="z-0" />
+
+      <div className="relative z-10">
+        <Portfolio />
+      </div>
+    </main>
+  );
 }
