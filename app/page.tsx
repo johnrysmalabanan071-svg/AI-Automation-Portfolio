987010f718a36bc4b472b93ef4bce80b627800ac
johnrysmalabanan071-svg <johnrysmalabanan071@gmail.com>
2026-09-17 20:34:35 +0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import { Portfolio } from "../components/portfolio";
+
+// The Portfolio component owns the single background and foreground layers.
+export default function HomePage() {
+  return <Portfolio />;
+}
